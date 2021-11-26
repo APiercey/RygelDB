ff29a04bcf05dab3c5a60b6fbd27fddab7761bd3
Alexander Piercey <alexander.piercey@gmail.com>
2021-11-26 01:41:19 +0100

    Create

---
+++ common/error_handling.go
@@ -0,0 +1,7 @@
+package common
+
+func HandleErr(err error) {
+  if err != nil {
+    panic(err2)
+  }
+}
--- core/store_repo.go
+++ core/store_repo.go
@@ -1,15 +1,48 @@
 package core
 
+import (
+  "os"
+  "rygel/common"
+)
+
 type StoreRepo struct {
-  Stores []*Store
+  Dir string
+  Stores []Store
 }
 
 func (sr StoreRepo) FindByName(name string) *Store {
   for _, store := range sr.Stores {
     if store.Name == name {
-      return store
+      return &store
     }
   }
 
   panic("Could not find store")
+}
+
+func (sr *StoreRepo) Create(name string) {
+  for _, store := range sr.Stores {
+    if store.Name == name {
+      panic("Store already exists")
+    }
+  }
+
+  f, err := os.Create(sr.Dir + name + ".store")
+
+  defer f.Close()
+
+  common.HandleErr(err)
+
+  _, err2 := f.WriteString(name + "\n")
+
+  common.HandleErr(err2)
+
+  store := BuildStore(name)
+
+  sr.Stores = append(sr.Stores, store)
+}
+
+func InitializeFromDir(dir string) StoreRepo {
+
+  return StoreRepo{Dir: dir}
 }
